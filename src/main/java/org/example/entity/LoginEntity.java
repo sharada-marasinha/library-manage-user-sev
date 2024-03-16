6a8ef052b5f49c97dd98cc75f0fe920f9b8a925c
sharada-marasinha <sharadamarasinha@gmail.com>
2024-03-16 15:13:26 +0530

    log in entity

--- src/main/java/org/example/entity/LoginEntity.java
+++ src/main/java/org/example/entity/LoginEntity.java
@@ -1,9 +1,6 @@
 package org.example.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
@@ -14,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name="LOGIN_TBL")
 public class LoginEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,7 +19,6 @@ public class LoginEntity {
     @NotEmpty(message = "Email cannot be empty")
     @Email(message = "Invalid email format")
     private String email;
-
     @NotEmpty(message = "Password cannot be empty")
     private String password;
 }
